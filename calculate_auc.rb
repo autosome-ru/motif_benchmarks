c1192c69322baf3a7dfa754fed0d24e2b053aa9d
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-01-25 17:55:47 +0300

    Initial commit

---
+++ calculate_auc.rb
@@ -0,0 +1,115 @@
+####  Input:
+####    >seqHeader:seqLength
+####    pvalue <TAB> any other info (e.g. position and orientation)
+####    >seqHeader:seqLength
+####    pvalue	position	orientation
+####  `seqHeader` has no constraints (such as uniqueness)
+#### Example:
+## >ANDR_HUMAN.PEAKS033090.pics.control:100
+## 0.001       5       -
+## >ANDR_HUMAN.PEAKS033090.pics.control:113
+## 3.5e-4       173     +
+
+require 'json'
+
+def median(arr)
+  raise 'Median of an empty array is undefined'  if arr.empty?
+  sorted_arr = arr.sort
+  if sorted_arr.size.odd?
+    center_index = sorted_arr.size / 2
+    sorted_arr[center_index]
+  else
+    center_index_ceiled = sorted_arr.size / 2
+    (sorted_arr[center_index_ceiled] + sorted_arr[center_index_ceiled - 1]) / 2.0
+  end
+end
+
+def correct_pvalues(pvalues, median_sequence_length:, model_length:)
+  pvalues.map{|pvalue|
+    1.0 - (1.0 - pvalue) ** (2 * (median_sequence_length - model_length + 1))
+  }
+end
+
+def calculate_auc(points, xy_keys: [0, 1])
+  xy_coords = points.map{|point| point.values_at(*xy_keys) }
+  xy_coords.sort_by{|x,y|
+    x
+  }.each_cons(2).map{|(x1, y1), (x2, y2)|
+    (x2 - x1) * (y1 + y2) / 2.0
+  }.inject(0.0, &:+)
+end
+
+def roc_curve(pvalues)
+  num_positive_sequences = pvalues.size # all sequences in dataset are positive
+
+  roc_points = []
+  roc_points << {tpr: 0.0, fpr: 0.0}
+  # list pvalues from the most stringent threshold (no one sequence taken as a positive one) to the weakest
+  sorted_pvalues = pvalues.sort
+  sorted_pvalues.each_with_index{|corrected_pvalue, index|
+    num_positive_sequences_taken = index + 1
+    tpr = num_positive_sequences_taken.to_f / num_positive_sequences
+    fpr = corrected_pvalue
+    roc_points << {tpr: tpr, fpr: fpr}
+  }
+  roc_points << {tpr: 1.0, fpr: 1.0}
+  roc_points
+end
+
+def round_values_in_curve(curve, *args)
+  curve.map{|point|
+    point.transform_values{|v| v.round(*args) }
+  }.chunk(&:itself).map(&:first) # that's uniq for consequent point
+end
+
+def read_motif_hits(stream)
+  sequence_lengths = []
+  pvalues = []
+  stream.each_line.lazy.map(&:chomp).each_slice(2).each{|seq_name, hit_info|
+    sequence_length = seq_name.split(':').last.to_i
+    pvalue = hit_info.split("\t").first.to_f
+    sequence_lengths << sequence_length
+    pvalues << pvalue
+  }
+  {sequence_lengths: sequence_lengths, pvalues: pvalues}
+end
+
+raise 'Specify model length'  unless model_length = Integer(ARGV[0])
+raise 'Specify file with model hits or `-` for stdin'  unless control_fn = ARGV[1]
+
+if control_fn == '-'
+  motif_hits = read_motif_hits($stdin)
+else
+  motif_hits = File.open(control_fn){|f| read_motif_hits(f) }
+end
+
+pvalues = correct_pvalues(motif_hits[:pvalues],
+                          median_sequence_length: median(motif_hits[:sequence_lengths]),
+                          model_length: model_length)
+
+roc = roc_curve(pvalues)
+logroc = roc
+  .reject{|point|
+    # we drop all the points which got negative corrected pvalue due to floating point errors
+    # and the point (0, 0) because log-transorming of zero is meaningless
+    point[:fpr] <= 0
+  }
+  .map{|point|
+    {logfpr: Math.log(point[:fpr]), tpr: point[:tpr]}
+  }
+
+roc_auc = calculate_auc(roc, xy_keys: [:fpr, :tpr])
+logroc_auc = calculate_auc(logroc, xy_keys: [:logfpr, :tpr])
+
+result = {
+  metrics: {
+    roc_auc: roc_auc,
+    logroc_auc: logroc_auc,
+  },
+  supplementary: {
+    roc: round_values_in_curve(roc, 2),
+    logroc: round_values_in_curve(logroc, 2),
+  },
+}
+
+puts result.to_json
---
+++ run.rb
@@ -0,0 +1,50 @@
+require 'json'
+require 'shellwords'
+
+def read_matrix(fn, num_columns: 4)
+  lines = File.readlines(fn).map(&:chomp)
+  rows = lines.map{|l| l.split("\t") }
+  name = File.basename(fn, File.extname(fn))
+  unless (rows[0].size == num_columns) && rows[0].all?{|x| Float(x, exception: false) }
+    hdr = lines.first
+    rows.shift
+    name = hdr.start_with?('>') ? hdr[1..-1].strip : hdr.strip
+  end
+  matrix = rows.map{|row|
+    row.map{|x| Float(x) }
+  }
+  raise  if matrix.empty?
+  raise  unless matrix.all?{|row| row.size == num_columns }
+  {name: name, matrix: matrix}
+end
+
+raise 'Specify config file' unless config_fn = ARGV[0]
+raise 'Specify results file' unless result_fn = ARGV[1]
+config = JSON.parse(File.read(config_fn))
+
+control_fn = '/data/control.formatted.mfa' # control should be formatted FASTA (i.e. with seq length specified in header line)
+pwm_fn = 'motif.pwm'
+mode = 'mono'
+File.write(pwm_fn, config['motif'])
+
+case mode
+when 'mono'
+  motif_length = read_matrix(pwm_fn, num_columns: 4)[:matrix].length
+  ape_class = 'ru.autosome.ape.PrecalculateThresholds'
+  sarus_class = 'ru.autosome.SARUS'
+when 'di'
+  motif_length = read_matrix(pwm_fn, num_columns: 16)[:matrix].length + 1
+  ape_class = 'ru.autosome.ape.di.PrecalculateThresholds'
+  sarus_class = 'ru.autosome.di.SARUS'
+else
+  raise 'Mode must be `mono` or `di`'
+end
+
+thresholds_fn = 'motif.thr'
+
+system("java -cp /app/ape.jar #{ape_class} #{pwm_fn.shellescape} --single-motif --background uniform > #{thresholds_fn.shellescape}")
+
+system("java -cp /app/sarus.jar #{sarus_class} #{control_fn.shellescape} #{pwm_fn.shellescape} besthit " + 
+    " --output-scoring-mode pvalue --pvalues-file #{thresholds_fn.shellescape} --add-flanks" +
+    " | ruby /app/calculate_auc.rb #{motif_length} - " +
+    " > #{result_fn.shellescape}")
