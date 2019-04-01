d1dfb7391110a8c7209e50429ca9a1c66f4dd68d
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-04-01 12:11:25 +0200

    fix

--- pwmeval_chipseq/download_assembly.sh
+++ pwmeval_chipseq/download_assembly.sh
@@ -2,5 +2,5 @@
 set -e -u -o pipefail
 ASSEMBLY_NAME=$1
 mkdir -p /tmp/assembly_${ASSEMBLY_NAME}/
-rsync -a -P rsync://hgdownload.soe.ucsc.edu/goldenPath/${ASSEMBLY_NAME}/chromosomes/  /tmp/assembly_${ASSEMBLY_NAME}/
+rsync -a -P rsync://hgdownload.soe.ucsc.edu/goldenPath/${ASSEMBLY_NAME}/chromosomes/*.fa.gz  /tmp/assembly_${ASSEMBLY_NAME}/
 zcat /tmp/assembly_${ASSEMBLY_NAME}/*.fa.gz
