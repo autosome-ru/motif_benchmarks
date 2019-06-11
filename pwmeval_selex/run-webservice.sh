d5755a9f5d5542e617a6bd2e27ee504cf4465564
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-06-11 14:22:16 +0300

    tmp

--- pwmeval_selex/run-webservice.sh
+++ pwmeval_selex/run-webservice.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 /app/print_matrix_from_json.R /workdir/config.json pcm > /motif.pcm
 /app/print_matrix_from_json.R /workdir/config.json pfm > /motif.pfm
-Rscript /app/calculate_roc.R --json
+ln -s /data/common_data/selex_sequences.fa /seq.fa
+ln -s /results /workdir/persistent
+Rscript /app/calculate_roc.R --json --seed 13 > /workdir/persistent/result.json
