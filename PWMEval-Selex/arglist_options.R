bb6d81f2b7d20028fdd53329c33fa0568854d2e7
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-10-13 19:53:56 +0300

    pwm_selex: don't load json library unless necessary

--- PWMEval-Selex/arglist_options.R
+++ PWMEval-Selex/arglist_options.R
@@ -1,6 +1,6 @@
 arglist_sequence_options = list(
-  make_option(c("--seq"), dest= 'seq_fn', type='character', default=NA, help="Specify FASTA file"),
-  make_option(c("--seq-url"), dest= 'seq_url', type='character', default=NA, help="Use FASTA file located at some URL"),
+  make_option(c("--seq"), dest='seq_fn', type='character', default=NA, help="Specify FASTA file"),
+  make_option(c("--seq-url"), dest='seq_url', type='character', default=NA, help="Use FASTA file located at some URL"),
 
   make_option(c("--gz"), dest="compression_gz", default=FALSE, action="store_true", help="Force un-gzipping sequences"),
   make_option(c("--not-compressed"), dest="compression_no", default=FALSE, action="store_true", help="Prevent un-gzipping sequences"),
@@ -19,8 +19,8 @@ arglist_sequence_options = list(
 )
 
 arglist_motif_options = list(
-  make_option(c("--motif"), dest= 'motif_fn', type='character', default=NA, help="Specify motif file"),
-  make_option(c("--motif-url"), dest= 'motif_url', type='character', default=NA, help="Use PFM file located at some URL"),
+  make_option(c("--motif"), dest='motif_fn', type='character', default=NA, help="Specify motif file"),
+  make_option(c("--motif-url"), dest='motif_url', type='character', default=NA, help="Use PFM file located at some URL"),
   make_option(c("--pfm"), default=FALSE, action="store_true", help="Force use of PFM matrix"),
   make_option(c("--pcm"), default=FALSE, action="store_true", help="Force use of PCM matrix"),
   make_option(c("--pseudo-weight"), dest="pseudo_weight", type="double", default=0.0001, help="Set a pseudo-weight to re-normalize the frequencies of the positional-probability matrix (PFM) [default=%default]")
